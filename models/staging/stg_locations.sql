619223237cdbf469f562ceefea2c864df7b4081e
Michelle Baird <michelle.baird@fishtownanalytics.com>
2024-05-21 15:58:21 +0000

    fixed stg_locations

--- models/staging/stg_locations.sql
+++ models/staging/stg_locations.sql
@@ -3,10 +3,6 @@ with
 source as (
 
     select * from {{ source('jaffle_shop', 'stores') }}
-
-    -- if you generate a larger dataset,
-    -- you can limit the timespan to the current time with the following line
-    -- where ordered_at <= {{ var('truncate_timespan_to') }}
 ),
 
 renamed as (
